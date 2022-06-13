40d48742690a67be75b83fbc0e6b7695fdcb773a
sleepygloa <sleepygloa@gmail.com>
2022-06-13 17:55:47 +0900

    init

---
+++ src/main/java/com/comfunny/template_react_jpa/TemplateReactJpaApplication.java
@@ -0,0 +1,13 @@
+package com.comfunny.template_react_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateReactJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateReactJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comfunny/template_react_jpa/TemplateReactJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comfunny.template_react_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateReactJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
